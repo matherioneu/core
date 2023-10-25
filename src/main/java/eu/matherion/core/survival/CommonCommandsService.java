55a7bf59e222e7545acba0c34f571507f4e7b51f
Adam Volkman (maku) <itismaku@gmail.com>
2023-10-25 13:57:59 +0200

    feat(survival): rank command

--- src/main/java/eu/matherion/core/survival/CommonCommandsService.java
+++ src/main/java/eu/matherion/core/survival/CommonCommandsService.java
@@ -2,12 +2,27 @@ package eu.matherion.core.survival;
 
 import cz.maku.mommons.worker.annotation.BukkitCommand;
 import cz.maku.mommons.worker.annotation.Service;
+import eu.matherion.core.CoreApplication;
+import eu.matherion.core.shared.permissions.luckperms.LuckPermsDependency;
+import eu.matherion.core.shared.permissions.luckperms.LuckPermsDependencyProvider;
 import eu.matherion.core.shared.player.MatherionPlayer;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.model.group.Group;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.Node;
+import net.luckperms.api.node.NodeType;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 @Service(commands = true)
@@ -90,5 +105,47 @@ public class CommonCommandsService {
         matherionPlayer.setLocalValue("nightvision", true);
         player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
         player.sendMessage("§8§l! §aZapnul §7sis noční vidění.");
+    }
+
+    @BukkitCommand(value = "rank", aliases = "rankexpirace")
+    public void onRankCommand(CommandSender sender) {
+        if (!(sender instanceof Player player)) return;
+        LuckPermsDependency luckPermsDependency = CoreApplication.getDependency(LuckPermsDependencyProvider.class);
+        if (luckPermsDependency == null) {
+            player.sendMessage("§4§l! §cChyba, LuckPermsDependency je null.");
+            return;
+        }
+        LuckPerms luckPerms = luckPermsDependency.get();
+        if (luckPerms == null) {
+            player.sendMessage("§4§l! §cChyba, LuckPerms je null.");
+            return;
+        }
+        User user = luckPerms.getUserManager().getUser(player.getName());
+        if (user == null) {
+            player.sendMessage("§4§l!Chyba, uživatel je null.");
+            return;
+        }
+        List<Instant> expire = user.getNodes(NodeType.INHERITANCE)
+                .stream()
+                .filter(Node::hasExpiry)
+                .map(Node::getExpiry)
+                .toList();
+        if (expire.isEmpty()) {
+            player.sendMessage("§8§l! §7Nemáš aktivovaný žádný prémiový rank.");
+            return;
+        }
+        expire.forEach(instant -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+                    .withLocale(Locale.GERMANY)
+                    .withZone(ZoneId.systemDefault());
+            String time = formatter.format(instant).replace(",", "");
+            Group group = luckPerms.getGroupManager().getGroup(user.getPrimaryGroup());
+            if (group == null) return;
+            String prefix = group.getCachedData().getMetaData().getPrefix();
+            if (prefix == null) {
+                prefix = group.getName();
+            }
+            player.sendMessage(String.format("§8§l! §7Tvůj rank §f%s §7vyprší §f%s§7.", ChatColor.translateAlternateColorCodes('&', prefix), time));
+        });
     }
 }
